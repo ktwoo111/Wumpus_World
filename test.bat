a819c9b277f80d53c1efc1ab212c5127202d4bcb
Taewoo Kim <dkdldzk@gmail.com>
2018-04-02 11:26:41 -0600

    made batch file for test cases.

---
+++ test.bat
@@ -0,0 +1,9 @@
+python check_true_false.py Tests/testKB.txt Tests/TestAddKnow.txt Tests/defTrue.txt
+
+python check_true_false.py Tests/testKB.txt Tests/TestAddKnow.txt Tests/defFalse.txt
+
+python check_true_false.py Tests/wrongTestKB.txt Tests/TestAddKnow.txt Tests/bothTrueFalse.txt
+
+python check_true_false.py Tests/testKB.txt Tests/TestAddKnow.txt Tests/possibleTrueFalse.txt
+
+PAUSE
