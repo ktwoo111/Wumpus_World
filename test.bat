02e051ed17887a6523708bf33820a5e95a5a5375
Tae <tkim@mines.edu>
2018-04-02 19:54:20 -0600

    I think it looks pretty good now. Waiting further testing from friend's code.

--- test.bat
+++ test.bat
@@ -1,3 +1,5 @@
+python check_true_false.py a.txt b.txt c.txt
+
 python check_true_false.py Tests/testKB.txt Tests/TestAddKnow.txt Tests/defTrue.txt
 
 python check_true_false.py Tests/testKB.txt Tests/TestAddKnow.txt Tests/defFalse.txt
@@ -5,5 +7,7 @@ python check_true_false.py Tests/testKB.txt Tests/TestAddKnow.txt Tests/defFalse
 python check_true_false.py Tests/wrongTestKB.txt Tests/TestAddKnow.txt Tests/bothTrueFalse.txt
 
 python check_true_false.py Tests/testKB.txt Tests/TestAddKnow.txt Tests/possibleTrueFalse.txt
+
+python check_true_false.py wumpus_rules.txt AddKB.txt statement.txt
 
 PAUSE
